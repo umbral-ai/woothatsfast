6e91bd620724e02ba2f868f7e6ac11e43291b04a
Artisan <daniel@domartisan.com>
2025-01-15 16:28:13 -0500

    cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
-   vite: {
-    plugins: [tailwindcss()],
-  },
-   site: 'https://yourdomain.com',
-  integrations: [sitemap()]
+  vite: {
+   plugins: [tailwindcss()],
+ },
+
+  site: 'https://yourdomain.com',
+  integrations: [sitemap()],
+  adapter: cloudflare()
 });
